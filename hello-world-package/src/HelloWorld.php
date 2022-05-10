07963704639a7540653142e08ffde44faa774560
cliff gardner <cliff@csgardner.com>
2022-05-10 13:32:54 -0400

    second commit

---
+++ hello-world-package/src/HelloWorld.php
@@ -0,0 +1,12 @@
+<?php
+// tutorial : https://dudi.dev/create-composer-php-package
+
+namespace HelloWorldPackage;
+
+class HelloWorld
+{
+    public static function sayHello()
+    {
+        echo "Hello World";
+    }
+}
